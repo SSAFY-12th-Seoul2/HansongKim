3199e66e18c3cdb5fa649ca2915dcb883a243818
김한송 <knhs4@naver.com>
2024-07-25 17:59:47 +0900

    fixed

--- boj/boj10163/boj10163.py
+++ boj/boj10163/boj10163.py
@@ -2,22 +2,20 @@ import sys
 
 n = int(input())
 xywh_lst = [[] for _ in range(n)]
-set_list = [[] for _ in range(n)]
-sum_set = []
+set_list = [0 for _ in range(n)]
+sum_set = {}
 for num in range(n):
     xywh_lst[num] = list(map(int, sys.stdin.readline().split()))
 
 for num in range(n-1, -1, -1):
     x, y, wid, hei = xywh_lst[num]
+    cnt = 0
     for i in range(x, x + wid):
         for j in range(y, y + hei):
-            if num == (n - 1):
-                set_list[num].append((i, j))
-                sum_set += [(i, j)]
-            else:
-                if (i, j) not in sum_set:
-                    set_list[num].append((i, j))
-                    sum_set += [(i, j)]
-print(sum_set)
+            if sum_set.get((i, j)) is None:
+                sum_set[(i, j)] = 1
+                cnt += 1
+    set_list[num] = cnt
+
 for k in range(n):
-    print(len(set_list[k]))
+    print(set_list[k])
