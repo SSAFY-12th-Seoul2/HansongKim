5aaf8abdaea378171c0bbfd57f6d6d70022fc3b7
김한송 <knhs4@naver.com>
2024-07-26 02:19:30 +0900

    solved

--- boj/boj10157/boj10157.py
+++ boj/boj10157/boj10157.py
@@ -3,20 +3,34 @@ num = int(input())
 if num > c * r:
     print(0)
 else:
-    ft = 2
+    start = 0
+    end = 0
     n = 1
-    bt = 4
-    line = 0
+    line = 1
     while True:
-        line += 1
-        dir = ft * (r + c) - bt
-        if dir > num:
+        start = end + 1
+        end += 2 * (r + c) - 4 * n
+        if num >= start and num <= end:
             break
 
-        ft += 2
+        line += 1
         n += 2
-        bt += (4 * n)
-        print(line, ft, bt, dir)
 
-    ft -= 2
-    bt -= (4 * n)
+    sub = 2 * (line - 1) + 1
+    at_point = [line, line]
+    if num == start:
+        print(*at_point)
+    elif (num - start) <= (r - sub):
+        at_point[1] += (num - start)
+        print(*at_point)
+    elif (num - start) <= (r + c - 2*sub):        
+        at_point[0] += (num - (start + r - sub))
+        at_point[1] += (r - sub)
+        print(*at_point)
+    elif (num - start) <= (2*r + c - 3*sub):
+        at_point[0] += (c - sub)
+        at_point[1] += (start - num + 2*r + c - 3*sub)
+        print(*at_point)
+    elif num <= end:
+        at_point[0] += (start - num + 2*r + 2*c - 4*sub)
+        print(*at_point)
