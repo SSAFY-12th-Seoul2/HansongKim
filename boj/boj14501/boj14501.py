d6d7a7eb3dfb80d9f71a3d04b0c1f0c93f25d31b
김한송 <knhs4@naver.com>
2024-07-29 01:29:33 +0900

    tried

---
+++ boj/boj14501/boj14501.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(input())
+n_list = [[] for _ in range(n+1)]
+for i in range(1, n+1):
+    work, wage = map(int, sys.stdin.readline().split())
+    n_list[i] = [i, work - 1, wage]
+
+while True:
+    if n_list[0]
+    pass
